2e0776873fc92aad6024c8b8f0ec0de4bc8042dc
Eduardo Chero <eduardochero08@gmail.com>
2023-09-22 19:15:01 -0500

    Validaciones y conexion a la base de datos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Trabajo_Finanzas_V1.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BdFianzasContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("conexion"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 var app = builder.Build();
 
