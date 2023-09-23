c9f32fc0c76e9f64d3d6258b24e6d904b7cb038d
Eduardo Chero <eduardochero08@gmail.com>
2023-09-22 19:13:50 -0500

    Validaciones del login y registro en el controller (falta arreglar)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using Trabajo_Finanzas_V1.Models;
 
@@ -7,10 +8,12 @@ namespace Trabajo_Finanzas_V1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BdFianzasContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BdFianzasContext context)
         {
             _logger = logger;
+            _context = context;
         }
         public IActionResult PlanDePagos()
         {
@@ -34,6 +37,41 @@ namespace Trabajo_Finanzas_V1.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public Task<IActionResult> Login(int id, string correo, string contrasena)
+        {
+            try
+            {
+                var cliente = _context.Clientes.FirstOrDefault(x => x.IdUser == id && x.CorreoElectronico == correo && x.Contrasena == contrasena);
+                return Task.FromResult<IActionResult>(RedirectToAction("PlanDePagos", "Home"));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return Task.FromResult<IActionResult>(View());
+            }
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Nombre,Apellido,Dni,Telefono,CorreoElectronico,Contrasena")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
